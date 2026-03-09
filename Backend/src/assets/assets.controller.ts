da10c1f699c70ba9338d9d43e7c3022f8e4e27c1
TimelyCo <anmolraturi25@gmail.com>
2026-03-09 23:17:04 +0530

    Get Assets Assigned to a User was added to the asset folder

--- Backend/src/assets/assets.controller.ts
+++ Backend/src/assets/assets.controller.ts
@@ -1,28 +1,43 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { AssetsService } from './assets.service';
 import { CreateAssetDto } from './dto/create-asset.dto';
+import { Patch } from '@nestjs/common';
+import { AssignAssetDto } from './dto/assign-asset.dto';
 
 @Controller('assets')
 export class AssetsController {
-  constructor(private readonly assetsService: AssetsService) {}
+    constructor(private readonly assetsService: AssetsService) { }
 
-  @Post()
-  create(@Body() createAssetDto: CreateAssetDto) {
-    return this.assetsService.create(createAssetDto);
-  }
+    @Post()
+    create(@Body() createAssetDto: CreateAssetDto) {
+        return this.assetsService.create(createAssetDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.assetsService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.assetsService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.assetsService.findOne(id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        return this.assetsService.findOne(id);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.assetsService.remove(id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+        return this.assetsService.remove(id);
+    }
+
+    @Patch(':id/assign')
+    assignAsset(
+        @Param('id') id: number,
+        @Body() assignAssetDto: AssignAssetDto,
+    ) {
+        return this.assetsService.assignAsset(id, assignAssetDto.userId);
+    }
+
+    @Get('user/:userId')
+    getAssetsByUser(@Param('userId') userId: number) {
+        return this.assetsService.findAssetsByUser(userId);
+    }
 }
--- Backend/src/assets/assets.service.ts
+++ Backend/src/assets/assets.service.ts
@@ -6,25 +6,44 @@ import { CreateAssetDto } from './dto/create-asset.dto';
 
 @Injectable()
 export class AssetsService {
-  constructor(
-    @InjectRepository(Asset)
-    private assetRepository: Repository<Asset>,
-  ) {}
+    constructor(
+        @InjectRepository(Asset)
+        private assetRepository: Repository<Asset>,
+    ) { }
 
-  create(createAssetDto: CreateAssetDto) {
-    const asset = this.assetRepository.create(createAssetDto);
-    return this.assetRepository.save(asset);
-  }
+    create(createAssetDto: CreateAssetDto) {
+        const asset = this.assetRepository.create(createAssetDto);
+        return this.assetRepository.save(asset);
+    }
 
-  findAll() {
-    return this.assetRepository.find();
-  }
+    findAll() {
+        return this.assetRepository.find();
+    }
 
-  findOne(id: number) {
-    return this.assetRepository.findOneBy({ id });
-  }
+    findOne(id: number) {
+        return this.assetRepository.findOneBy({ id });
+    }
 
-  remove(id: number) {
-    return this.assetRepository.delete(id);
-  }
+    remove(id: number) {
+        return this.assetRepository.delete(id);
+    }
+
+    async assignAsset(assetId: number, userId: number) {
+        const asset = await this.assetRepository.findOneBy({ id: assetId });
+
+        if (!asset) {
+            throw new Error('Asset not found');
+        }
+
+        asset.assigned_to = userId;
+        asset.status = 'ASSIGNED';
+
+        return this.assetRepository.save(asset);
+    }
+
+    async findAssetsByUser(userId: number) {
+        return this.assetRepository.find({
+            where: { assigned_to: userId },
+        });
+    }
 }
